af1bca03f4298ddb42f2a43f15b6c9400e0a8356
Csíkos Csaba <84637366+robotTX1@users.noreply.github.com>
2025-09-27 10:23:21 +0200

    New workflow (#13)
    Changes

--- src/main/java/com/robottx/githubactions/GithubActionsTestApplication.java
+++ src/main/java/com/robottx/githubactions/GithubActionsTestApplication.java
@@ -1,5 +1,6 @@
 package com.robottx.githubactions;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -11,6 +12,16 @@ public class GithubActionsTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubActionsTestApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean("test")
+	public ObjectMapper getObjectMapper() {
+		return new ObjectMapper();
 	}
 
 }
